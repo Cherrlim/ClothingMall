95a467d8e9985660f4dc4e77cab2668c3782d1b4
Lgelo <2628977286@qq.com>
2024-06-22 10:10:24 +0800

    DAO

---
+++ src/main/java/com/entity/vo/YanseVO.java
@@ -0,0 +1,21 @@
+package com.entity.vo;
+
+import com.entity.YanseEntity;
+
+import com.baomidou.mybatisplus.annotations.TableName;
+import java.util.Date;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.io.Serializable;
+ 
+
+/**
+ * 颜色
+ * 手机端接口返回实体辅助类
+ */
+public class YanseVO  implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	 				
+}
